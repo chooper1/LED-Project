510d9869136816fbfd5442fbadca1064e891f1ed
chooper1 <54963573+chooper1@users.noreply.github.com>
2020-03-24 21:51:33 -0300

    pseudo1

---
+++ Software/readScannerID_pseudocode.py
@@ -0,0 +1,5 @@
+# this function accesses the ID of the rf scanner when it is used
+# it will likely have to be triggered by the action of scanning or it will have to be continuously polling
+def readScannerID():
+    # RFID = value read from WMS (most recently used )_
+    return RFID
