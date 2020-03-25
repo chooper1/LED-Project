083856bae882f6036a9a874d41e2d0f5258a4f2d
chooper1 <54963573+chooper1@users.noreply.github.com>
2020-03-24 22:49:32 -0300

    Update

--- Software/sendPartLocationAndRFID_pseudocode.py
+++ Software/sendPartLocationAndRFID_pseudocode.py
@@ -1,6 +1,7 @@
 # this function sends the location and RFID to the microcontroller
 
-def sendPartLocationAndRFID(location, RFID):
+def sendPartLocationAndRFID(location, RFID, arduinoID):
+    #FIRST: need to translate based on th
     #use built in library to transmit location to the arduino
     #use built in library to transmit RFID to the arduino
     #the arduino should block while waiting for these two commands
