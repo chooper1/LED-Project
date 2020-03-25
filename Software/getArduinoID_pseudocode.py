a9241ff6fb045be51aefcdcffa34c6d59c86da4d
chooper1 <54963573+chooper1@users.noreply.github.com>
2020-03-24 22:52:59 -0300

    Create getArduinoID

---
+++ Software/getArduinoID_pseudocode.py
@@ -0,0 +1,6 @@
+# this function gets the arduinoID corresponding to a given location
+
+def getArduinoID(location):
+    # index into some sort of table or database to get the arduinoID
+    # arduinoID = table(location)
+    return arduinoID
