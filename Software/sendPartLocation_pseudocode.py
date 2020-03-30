ab4b7a27e7d6bf3a8dede40dad93d1a6eb9b6d8c
chooper1 <54963573+chooper1@users.noreply.github.com>
2020-03-30 19:36:45 -0300

    Delete sendPartLocation_pseudocode.py

--- Software/sendPartLocation_pseudocode.py
+++
@@ -1,6 +0,0 @@
-# this function sends the location to the microcontroller (without RFID)
-
-def sendPartLocation(location, ON_OFF, arduinoID):
-    #use built in library to transmit location to the arduino corresponding to arduinoID
-    #use built in library to transmit ON_OFF to the arduino corresponding to arduinoID
-    #the arduino should block while waiting for these two commands
