1aeb242b13d86b108c6e5fc07d605385f1170503
chooper1 <54963573+chooper1@users.noreply.github.com>
2020-04-13 19:38:55 -0300

    Update findNewTransactions.py

--- Software/findNewTransaction.py
+++ Software/findNewTransaction.py
@@ -1,16 +1,14 @@
 # function to detect when a new transaction occurs that outputs the transaction's information 
 
-def findNewTransaction():
-
-	# continuously loop through the most recent 50 transactions in the table 
-	# store the top 5 transactions in a locally stored SQL table
-		# start with an empty table for the first ever transaction (so that we don't miss the first 5)
-		# then table will fill up as transactions come in 
-	# compare the newest transaction with the transactions in the SQL table 
-	# if the transaction is already in the table
-		# return false 
-	# if the transaction is not already in the table
-		# save it in the table 
-		# delete the oldest transaction in the table 
-		# output the needed information from the transaction (pick or put, location, RFID)
-		# return transaction_type, location, RFID, true 
+def findNewTransaction(prevTransaction):
+	if(len(prevTransaction) == 0): # no previous transactions
+		#access all transactions that are waiting to be putaway / waiting to be picked
+		newTrans = query transactionTable # need to replace this with sql query for all newer objects
+        else:
+		#access all transactions newer than prevTransaction that are waiting to be putaway / waiting to be picked
+		newTrans = query transactionTable # need to replace this with sql query for all newer objects
+        
+	newTransactions = []
+	for i in range(0,len(newTrans)):
+		newTransactions.append(newTrans[i]) # assumes newTrans[i] is formatted like (pickOrPut, location, RFID)
+	return newTransactions # should be [] if no new transactions
