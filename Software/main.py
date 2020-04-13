70ac845cedc008a00771e1ca8d1cc63a2f5ccbd4
chooper1 <54963573+chooper1@users.noreply.github.com>
2020-04-13 16:40:28 -0300

    Update main.py (rough outline)

--- Software/main.py
+++ Software/main.py
@@ -4,15 +4,47 @@
 from getArduinoID import getArduinoID 
 from sendPartLocation import sendPartLocation
 
+# coding: utf-8
+# In[ ]:
+from socket import *
+import time
+
+
 def main():
-	
-    while(1):
-        pickOrPut, location, RFID = findNewTransaction()
+	prevTransaction = [] 
+    	while(1):
+		#findNewTransaction() needs to be updated to return multiple transactions, and needs to be able to take in either
+		#the previously processed transaction or no transactions
+		#pickOrPut, location, RFID = findNewTransaction()
+		newTransactions = findNewTransaction(prevTransaction)
+		
+		if(len(newTransactions) > 0):
+			for i in range(0,len(newTransactions)):
+				pickOrPut, location, RFID = newTransactions[i] 
 
-        sendPartLocation(location)
+				address = getIP(location)
+				#address = ('192.168.5.27', 5000) # Define who you are talking to
+				client_socket = socket(AF_INET, SOCK_DGRAM) #set up the socket
+				client_socket.settimeout(1) #only wait one second for a response
 
-        getArduinoID(location)
-	
+				ON = onOrOff(location, RFID, pickOrPut)
+
+				#sendPartLocationAndRFID(location, RFID, IP)
+
+				#input format: [storage location,pickerID,on/off]
+				string_address = input() # ask for user input
+				data = string_address.encode('utf-8') # encode input as a byte for datagram protocol
+
+				client_socket.sendto(data,address) #send command to arduino
+				try:
+					rec_data, addr = client_socket.recvfrom(2048) #reading response from arduino 
+					#print(rec_data.decode()) #Print the response from the arduino
+				except:
+					#error checking if transmission doesn't work?
+					pass
+				client_socket.close()
+			
+			prevTransaction = newTransactions[len(newTransactions)-1]
 
 if __name__== "__main__":
-    main()
+	main()
