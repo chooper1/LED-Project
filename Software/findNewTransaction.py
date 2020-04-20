467b21e293fbeafe6211819f212e14c6eb5089e1
chooper1 <54963573+chooper1@users.noreply.github.com>
2020-04-20 01:53:26 -0300

    Update newTransactions

--- Software/findNewTransaction.py
+++ Software/findNewTransaction.py
@@ -1,14 +1,25 @@
 # function to detect when a new transaction occurs that outputs the transaction's information 
 
-def findNewTransaction(prevTransaction):
-	if(len(prevTransaction) == 0): # no previous transactions
-		#access all transactions that are waiting to be putaway / waiting to be picked
-		newTrans = query transactionTable # need to replace this with sql query for all newer objects
-        else:
-		#access all transactions newer than prevTransaction that are waiting to be putaway / waiting to be picked
-		newTrans = query transactionTable # need to replace this with sql query for all newer objects
-        
-	newTransactions = []
-	for i in range(0,len(newTrans)):
-		newTransactions.append(newTrans[i]) # assumes newTrans[i] is formatted like (pickOrPut, location, RFID)
+def findNewTransaction(prevTransactionNumber):
+	import sqlite3
+	import numpy 
+	
+	conn = sqlite3.connect('wms.db')
+	cursor = conn.cursor()
+	table_name = 'transaction'
+	id_column = 'test' #not primary key, since two transactions could happen in same time step
+	column_2 = 'pickOrPut' 
+	column_3 = 'location'
+	column_4 = 'RFID' 
+    	# https://sebastianraschka.com/Articles/2014_sqlite_in_python_tutorial.html#querying-the-database---selecting-rows
+    	c.execute("SELECT {coi1},{coi2},{coi3} FROM {tn} WHERE {idf}={my_id}".\
+      	      format(tn=table_name, coi1=column_2, coi2=column3, coi3=column4; idf=id_column, my_id=prevTransactionNumber))
+	
+	rows = c.fetchall()
+	if (rows):
+		newTransactions = numpy.array(rows) 
+	else: 
+		newTransactions = []
+	conn.close()
+    
 	return newTransactions # should be [] if no new transactions
